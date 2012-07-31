fdec43338a66f37b65da3a50370285998900362b
中野 麻美 <nakano@nakano-no-MacBook-Air.local>
2012-07-31 23:07:23 +0900

    Initial Commit

---
+++ OOP_CalcTests/OOP_CalcTests.h
@@ -0,0 +1,13 @@
+//
+//  OOP_CalcTests.h
+//  OOP_CalcTests
+//
+//  Created by 中野 麻美 on 12/07/31.
+//  Copyright (c) 2012年 中野 麻美. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface OOP_CalcTests : SenTestCase
+
+@end
---
+++ OOP_CalcTests/OOP_CalcTests.m
@@ -0,0 +1,32 @@
+//
+//  OOP_CalcTests.m
+//  OOP_CalcTests
+//
+//  Created by 中野 麻美 on 12/07/31.
+//  Copyright (c) 2012年 中野 麻美. All rights reserved.
+//
+
+#import "OOP_CalcTests.h"
+
+@implementation OOP_CalcTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in OOP_CalcTests");
+}
+
+@end

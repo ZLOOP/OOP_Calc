6418023f1c28bed5cb2d92a51a4e544f22049cdc
A.Nakano <q9531045@eng.kitakyu-u.ac.jp>
2012-08-17 19:40:47 +0900

    storyboardで，画面のインターフェイスを作成．

--- OOP_CalcTests/OOP_CalcTests.h
+++ OOP_CalcTests/OOP_CalcTests.h
@@ -2,9 +2,6 @@
 //  OOP_CalcTests.h
 //  OOP_CalcTests
 //
-//  Created by 中野 麻美 on 12/07/31.
-//  Copyright (c) 2012年 中野 麻美. All rights reserved.
-//
 
 #import <SenTestingKit/SenTestingKit.h>
 
--- OOP_CalcTests/OOP_CalcTests.m
+++ OOP_CalcTests/OOP_CalcTests.m
@@ -2,9 +2,6 @@
 //  OOP_CalcTests.m
 //  OOP_CalcTests
 //
-//  Created by 中野 麻美 on 12/07/31.
-//  Copyright (c) 2012年 中野 麻美. All rights reserved.
-//
 
 #import "OOP_CalcTests.h"
 

d4d671ee03591ec82a58087fc493ae6d5067e2ea
A.Nakano <q9531045@eng.kitakyu-u.ac.jp>
2012-08-18 22:56:24 +0900

    addMethodsに仕様のメソッドを形だけ作成

--- OOP_Calc/addMethods.h
+++ OOP_Calc/addMethods.h
@@ -5,6 +5,21 @@
 
 #import <Foundation/Foundation.h>
 
-@interface addMethods : NSObject
+@interface addMethods : NSObject{
+}
+
+#pragma mark - 初期化
+- (void)allclear;
+
+#pragma mark - 入力関係
+- (NSString *)setNumber:(NSNumber *)value;      //数字用
+- (void)setOperation:(NSNumber *)value;         //演算子用
+- (NSString *)setPoint;                         //小数点用
+- (NSString *)turnSign;                         //符号用
+
+- (void)substituteViewValue:(NSString *)temp ForArg:(NSNumber *)num;    //arg1か2にtempを代入する
+
+#pragma mark - その他
+- (NSString *)calculation;  //計算をする
 
 @end

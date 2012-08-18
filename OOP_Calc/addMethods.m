ea0fa261da66147ae53b41ba515090d4d60a9b34
A.Nakano <q9531045@eng.kitakyu-u.ac.jp>
2012-08-18 17:05:00 +0900

    addMethodを作成．

---
+++ OOP_Calc/addMethods.m
@@ -0,0 +1,13 @@
+//
+//  addMethods.m
+//  OOP_Calc
+//
+//  Created by 中野 麻美 on 12/08/18.
+//  Copyright (c) 2012年 中野 麻美. All rights reserved.
+//
+
+#import "addMethods.h"
+
+@implementation addMethods
+
+@end

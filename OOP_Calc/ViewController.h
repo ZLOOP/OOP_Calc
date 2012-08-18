da20e68c1e3d258aa718f518ee341e4135064f4a
A.Nakano <q9531045@eng.kitakyu-u.ac.jp>
2012-08-18 20:07:53 +0900

    全てのトリガーの宣言

--- OOP_Calc/ViewController.h
+++ OOP_Calc/ViewController.h
@@ -10,4 +10,26 @@
     __weak IBOutlet UILabel *valueViewLabrl;
 }
 
+//数字のトリガー
+- (IBAction)number0Button_down:(id)sender;      // 0
+- (IBAction)number1Button_down:(id)sender;      // 1
+- (IBAction)number2Button_down:(id)sender;      // 2
+- (IBAction)number3Button_down:(id)sender;      // 3
+- (IBAction)number4Button_down:(id)sender;      // 4
+- (IBAction)numberButton5_down:(id)sender;      // 5
+- (IBAction)number6Button_down:(id)sender;      // 6
+- (IBAction)number7Button_down:(id)sender;      // 7
+- (IBAction)number8Button_down:(id)sender;      // 8
+- (IBAction)number9Button_down:(id)sender;      // 9
+//演算子のトリガー
+- (IBAction)addButton_down:(id)sender;          // +
+- (IBAction)subButton_down:(id)sender;          // -
+- (IBAction)mulButton_down:(id)sender;          // ×
+- (IBAction)divButton_down:(id)sender;          // ÷
+//その他のトリガー
+- (IBAction)signButton_down:(id)sender;         // +/-
+- (IBAction)pointButton_down:(id)sender;        // .
+- (IBAction)equalButton_down:(id)sender;        // =
+- (IBAction)allclearButton_down:(id)sender;     // AC
+
 @end
--- OOP_Calc/ViewController.m
+++ OOP_Calc/ViewController.m
@@ -29,4 +29,55 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+- (IBAction)number0Button_down:(id)sender {
+}
+
+- (IBAction)number1Button_down:(id)sender {
+}
+
+- (IBAction)number2Button_down:(id)sender {
+}
+
+- (IBAction)number3Button_down:(id)sender {
+}
+
+- (IBAction)number4Button_down:(id)sender {
+}
+
+- (IBAction)numberButton5_down:(id)sender {
+}
+
+- (IBAction)number6Button_down:(id)sender {
+}
+
+- (IBAction)number7Button_down:(id)sender {
+}
+
+- (IBAction)number8Button_down:(id)sender {
+}
+
+- (IBAction)number9Button_down:(id)sender {
+}
+- (IBAction)addButton_down:(id)sender {
+}
+
+- (IBAction)subButton_down:(id)sender {
+}
+
+- (IBAction)mulButton_down:(id)sender {
+}
+
+- (IBAction)divButton_down:(id)sender {
+}
+- (IBAction)signButton_down:(id)sender {
+}
+
+- (IBAction)pointButton_down:(id)sender {
+}
+
+- (IBAction)equalButton_down:(id)sender {
+}
+
+- (IBAction)allclearButton_down:(id)sender {
+}
 @end
